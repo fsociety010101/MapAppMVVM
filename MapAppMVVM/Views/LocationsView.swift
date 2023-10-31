a902c4f0ca76973382e59ad9e1d8dca744aa25f4
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-31 19:10:39 +0100

    updated locations view with view model

--- MapAppMVVM/Views/LocationsView.swift
+++ MapAppMVVM/Views/LocationsView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct LocationsView: View {
     
+    @EnvironmentObject private var vm: LocationsViewModel
     
     var body: some View {
         List {
+            ForEach(vm.locations) {
+                Text($0.name)
+            }
         }
     }
 }
 
 #Preview {
     LocationsView()
+        .environmentObject(LocationsViewModel())
 }
