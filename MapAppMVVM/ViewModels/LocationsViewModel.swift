d928240d807a602080bf6ab0dd2f8d4133080e22
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-31 19:31:37 +0100

    added published properties to view model
    current map location and map region

--- MapAppMVVM/ViewModels/LocationsViewModel.swift
+++ MapAppMVVM/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+import MapKit
+
 
 class LocationsViewModel: ObservableObject {
     
+    // all loaded locations
     @Published var locations: [Location]
+    
+    // current location on the map
+    @Published var mapLocation: Location {
+    }
+    
+    // current map region
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    // default map span 
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        
+        self.mapLocation = locations.first!
+        
     }
 }
