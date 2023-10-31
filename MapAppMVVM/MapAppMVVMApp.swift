57d5219f4f4698026d5e9165319a0bcc5fa5c8e5
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-31 19:07:58 +0100

    added main entry view with locations

--- MapAppMVVM/MapAppMVVMApp.swift
+++ MapAppMVVM/MapAppMVVMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MapAppMVVMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LocationsView()
         }
     }
 }
---
+++ MapAppMVVM/Views/LocationsView.swift
@@ -0,0 +1,21 @@
+//
+//  LocationsView.swift
+//  MapAppMVVM
+//
+//  Created by Dmytro Nimchynskyi on 31/10/2023.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    
+    
+    var body: some View {
+        List {
+        }
+    }
+}
+
+#Preview {
+    LocationsView()
+}
