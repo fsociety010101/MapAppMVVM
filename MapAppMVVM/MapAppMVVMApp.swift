0cf4979d3ee77a12852bca06fb3c1395c0f097b4
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-31 18:31:22 +0100

    Initial Commit

---
+++ MapAppMVVM/MapAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppMVVMApp.swift
+//  MapAppMVVM
+//
+//  Created by Dmytro Nimchynskyi on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
