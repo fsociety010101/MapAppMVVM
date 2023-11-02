fc66381af790101a584736adb0e38771651cfbbb
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-02 20:59:10 +0100

    code refactoring

--- MapAppMVVM/Views/LocationsView.swift
+++ MapAppMVVM/Views/LocationsView.swift
@@ -15,32 +15,17 @@ struct LocationsView: View {
     var body: some View {
         ZStack {
             
-            Map(coordinateRegion: $vm.mapRegion,
-                annotationItems: vm.locations,
-                annotationContent: { location in
-                MapMarker(coordinate: location.coordinates, tint: .blue)
-            })
-            .ignoresSafeArea()
+            mapLayer
+                .ignoresSafeArea()
             
             VStack(spacing: 0) {
+                
                 header
                     .padding()
                 
                 Spacer()
-                
-                ZStack {
-                    ForEach(vm.locations) { location in
-                        // to not render every location but only current one
-                        if vm.mapLocation == location {
-                            LocationPreviewView(location: location)
-                                .shadow(color: .black.opacity(0.3), radius: 20)
-                                .padding()
-                                .transition(.asymmetric(
-                                    insertion: .move(edge: .trailing),
-                                    removal: .move(edge: .leading)))
-                        }
-                    }
-                }
+
+                locationsPreviewStack
             }
         }
     }
@@ -79,5 +64,36 @@ extension LocationsView {
         .background(.thickMaterial)
         .cornerRadius(10)
         .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotaionView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                // to not render every location but only current one
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: .black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
 }
