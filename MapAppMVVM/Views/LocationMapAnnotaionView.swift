c037cf22987b14c01cf42b23dab4273f4ce6a571
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-02 20:40:28 +0100

    added custom map pins

---
+++ MapAppMVVM/Views/LocationMapAnnotaionView.swift
@@ -0,0 +1,44 @@
+//
+//  LocationMapAnnotaionView.swift
+//  MapAppMVVM
+//
+//  Created by Dmytro Nimchynskyi on 02/11/2023.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotaionView: View {
+    
+    let accentColor = Color("AccentColor")
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(6)
+                .background(accentColor)
+                .cornerRadius(36)
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(accentColor)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom, 40)
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        LocationMapAnnotaionView()
+    }
+}
